3da08fdd32efe1481cbea2076a1f06134162fc9e
Jeff Hutchison <jeff@jeffhutchison.com>
2015-01-06 10:48:19 -0500

    Add chefspec tests.

---
+++ spec/fail2ban_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'jhdc-firewall::fail2ban' do
+  let(:chef_run) {ChefSpec::SoloRunner.converge('jhdc-firewall::fail2ban')}
+
+  %w(fail2ban fail2ban-server fail2ban-systemd fail2ban-firewalld).each do |package|
+    it "installs #{package}" do
+      expect(chef_run).to upgrade_package(package)
+    end
+  end
+
+  it "enables and starts fail2ban servers" do
+    expect(chef_run).to enable_service('fail2ban')
+    expect(chef_run).to start_service('fail2ban')
+  end
+
+  it "creates local jail file" do
+    expect(chef_run).to render_file('/etc/fail2ban/jail.d/10-local.conf').with_content('enabled = true')
+  end
+end
