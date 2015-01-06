200ecd4a626d4b4b19aaf6c3e08e02a148e459ed
Jeff Hutchison <jeff@jeffhutchison.com>
2015-01-06 10:44:11 -0500

    Move LWRP to firewalld cookbook.

--- resources/port.rb
+++
@@ -1,6 +0,0 @@
-actions :add, :remove
-default_action :add
-
-attribute :port,     :kind_of => Integer, :name_attribute => true
-attribute :protocol, :kind_of => String,  :default => 'tcp'
-attribute :zone,     :kind_of => String
