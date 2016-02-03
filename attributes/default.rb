0009223807efebac6b54928d368f3e7955e2febd
Jeff Hutchison <jeff@jeffhutchison.com>
2016-02-03 08:44:51 -0500

    Clean up foodcritic warning.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 default['fail2ban']['services'] = {
   'sshd' => {
     'enabled' => 'true',
-    'bantime' => "#{10 * 60}" # 10 minutes is fail2ban default
+    'bantime' => "600" # 10 minutes is fail2ban default
   }
 }
