386764f92599fe550ca7b3df3e66304bbb05005f
Jeff Hutchison <jeff@jeffhutchison.com>
2016-03-06 08:12:54 -0500

    Resolve foodcritic warnings.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,10 @@ maintainer       'Jeff Hutchison'
 maintainer_email 'jeff@jeffhutchison.com'
 license          'Apache v2.0'
 description      'Installs/Configures firewall and fail2ban'
+source_url       'https://github.com/jhh/jhdc-firewall-cookbook'
+issues_url       'https://github.com/jhh/jhdc-firewall-cookbook/issues'
 version          '0.6.2'
+
 long_description <<EOT
 jhdc-firewall
 =============
